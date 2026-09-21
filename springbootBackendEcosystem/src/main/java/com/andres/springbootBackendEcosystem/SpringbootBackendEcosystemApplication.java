78d0f628f407c709c893f25faa88254c93931d31
Andres Castro <160083621+andres-n-castro@users.noreply.github.com>
2026-09-21 17:55:48 -0400

    DB dependencies within pom.xml were commented out. The reason being is that the server would not start unless there was a db url to connect and the postgres db has not been set up yet, Created a new controller for user called UserController.

--- springbootBackendEcosystem/src/main/java/com/andres/springbootBackendEcosystem/SpringbootBackendEcosystemApplication.java
+++ springbootBackendEcosystem/src/main/java/com/andres/springbootBackendEcosystem/SpringbootBackendEcosystemApplication.java
@@ -3,6 +3,7 @@ package com.andres.springbootBackendEcosystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringbootBackendEcosystemApplication {
 
