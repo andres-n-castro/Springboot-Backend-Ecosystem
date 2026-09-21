e0f3cbc179f3015cee3fe715ee39250693f494fa
Andres Castro <andresncastro12@gmail.com>
2026-09-21 02:24:32 -0400

    Created Maven Java project using Spring initializer with all required dependencies

---
+++ springbootBackendEcosystem/src/main/java/com/andres/springbootBackendEcosystem/SpringbootBackendEcosystemApplication.java
@@ -0,0 +1,13 @@
+package com.andres.springbootBackendEcosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendEcosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendEcosystemApplication.class, args);
+	}
+
+}
---
+++ springbootBackendEcosystem/src/test/java/com/andres/springbootBackendEcosystem/SpringbootBackendEcosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.springbootBackendEcosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendEcosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
